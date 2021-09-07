8aae0ddbc0ef26f5b4e76dd60ad14243506472f0
NavdeepGarg <gargnavdeep4@gmail.com>
2021-09-07 17:57:08 +0530

    Add files via upload

--- ReverseInGivenIndex.java
+++ ReverseInGivenIndex.java
@@ -3,7 +3,7 @@ package LinkedList;
 import javax.swing.plaf.synth.SynthSpinnerUI;
 
 class ListNode{
-	
+
 	int value;
 	ListNode next;
 	public ListNode(int value) {
@@ -11,19 +11,19 @@ class ListNode{
 		this.value = value;
 		this.next=null;
 	}
-	
-	
+
+
 }
 
 public class ReverseInGivenIndex {
-	
-	public static void revereLL(ListNode head){
-		
+
+	public static ListNode   revereLL(ListNode head){
+
 		ListNode current = head;
 		ListNode currentemp = head;
 		ListNode next = null;
 		ListNode prev=null;
-		
+
 		System.out.println("before");
 		while(currentemp!=null) {
 			System.out.print(currentemp.value+",");
@@ -31,12 +31,12 @@ public class ReverseInGivenIndex {
 		}
 		System.out.println("");
 		currentemp = head;
-		
+
 		while(current!=null) {
-		 next=current.next;
-		 current.next= prev;
-		 prev=current;
-		 current=next;
+			next=current.next;
+			current.next= prev;
+			prev=current;
+			current=next;
 		}
 		System.out.println("after");
 		currentemp=prev;
@@ -45,70 +45,108 @@ public class ReverseInGivenIndex {
 			currentemp=currentemp.next;
 		}
 		System.out.println("");
+		return prev;
 	}
 
-	
-	  public static void reverseBetween(ListNode head, int start, int end) {
-			
-			ListNode current = head;
-			ListNode currentemp = head;
-			ListNode next = null;
-			ListNode prev=null;
-			int startIndex=1;
-			while( startIndex<start) {
-				prev=current;
-				current=current.next;
-				 
-				startIndex++;
-			}
-			ListNode endref=current;
-			ListNode startref=current;
-			while(startIndex<end && endref!=null) {
-				endref= endref.next ;
-				startIndex++;
-			}
+	public static ListNode revereLL(ListNode head, int length){
+
+		ListNode current = head;
+		ListNode currentemp = head;
+		ListNode next = null;
+		ListNode prev=null;
+
+		System.out.println("before");
+		while(currentemp!=null) {
+			System.out.print(currentemp.value+",");
+			currentemp=currentemp.next;
+		}
+		System.out.println("");
+		currentemp = head;
+		int start=0;
+		while(current!=null && start<=length) {
+			next=current.next;
+			current.next= prev;
+			prev=current;
+			current=next;
+			start++;
+		}
+		System.out.println("after");
+		currentemp=prev;
+		while(currentemp!=null) {
+			System.out.print(currentemp.value+",");
+			currentemp=currentemp.next;
+		}
+		System.out.println("");
+		return prev;
+	}
+
+	public static ListNode reverseBetween(ListNode head, int start, int end) {
+
+		ListNode current = head;
+		ListNode currentemp = head;
+		ListNode next = null;
+		ListNode prev=current;
+		int startIndex=1;
+		ListNode startref= null;
+		
+		if(start==1) {
+			startref=new ListNode(-1);
+			startref.next=head;
+		}
+		else {
+		while( startIndex<start) {
 			prev=current;
 			current=current.next;
-			
-			while(start <end && current!=null && endref!=current) {
-				System.out.println("curr value "+current.value);
-				 next=current.next;
-				 current.next= prev;
-				 prev=current;
-				 current=next;
-				 start++;
-			}
-			
-			prev.next=endref;
-			int temp=startref.value;
-			startref.value= endref.value;
-			endref.value=temp;
-			
-			System.out.println("after");
-			currentemp=head;
-			while(currentemp!=null) {
-				System.out.print(currentemp.value+",");
-				currentemp=currentemp.next;
-			}
-		  
-		    
-	  } 
-	 
-	 public static void main(String[] args) {
+
+			startIndex++;
+		}
+		startref=prev;
+		}
+
 		
-		 
-		 ListNode node1= new ListNode(1);ListNode node2= new ListNode(2);
-		 node1.next=node2;
-		 ListNode node3= new ListNode(3);ListNode node4= new ListNode(4);ListNode node5= new ListNode(5);
-		 node2.next=node3;
-		 node3.next=node4;
-		 node4.next=node5;
-		 node5.next=null;
-		 
-		 
-		 
-	//	 revereLL(node1);
-		 reverseBetween(node1,1,4);
-		 
+		ListNode endref=current;
+	
+		while(startIndex<end && endref!=null) {
+			endref= endref.next ;
+			startIndex++;
+		}
+		endref=endref.next;
+
+
+		prev=revereLL(startref.next, end-start);
+		startref.next=prev;
+		while(prev.next!=null) {
+			prev= prev.next;
+		}
+		prev.next=endref;
+
+
+		//After required reversal;
+		System.out.println("After required reversal ");
+		ListNode temp = head;
+		while(temp!=null) {
+			System.out.print(temp.value+",");
+			temp=temp.next;
+		}
+
+		return head;
+	} 
+
+	public static void main(String[] args) {
+
+
+		ListNode node1= new ListNode(1);ListNode node2= new ListNode(2);
+		node1.next=node2;
+		ListNode node3= new ListNode(3);ListNode node4= new ListNode(4);ListNode node5= new ListNode(5);
+		node2.next=node3;
+		node3.next=node4;
+		node4.next=node5;
+		node5.next=null;
+
+
+
+		//	 revereLL(node1);
+		reverseBetween(node1,1,5);
+
 	}
 }
